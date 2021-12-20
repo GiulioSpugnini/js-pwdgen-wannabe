b85984c77f0ddc0eb3293de51bbc139183aad11e
GiulioSpugnini <giuliospugnini@yahoo.com>
2021-12-20 16:13:59 +0100

    added sum variables step

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 5. Chiedere il colore preferito all-utente
 6. Salvare il colore preferito in una variabile
 7. Sommare le variabili
+8. Stampo le variabili sommate
 */
 
 // Chiedo il nome all'utente
@@ -19,3 +20,6 @@ console.log(userSurname);
 // Chiedo qual è il colore preferito dell'utente
 const userFavouriteColor = prompt('Qual è il tuo colore preferito?');
 console.log(userFavouriteColor);
+
+// Sommo le variabili
+const variablesSum = userName + userSurname + userFavouriteColor;
