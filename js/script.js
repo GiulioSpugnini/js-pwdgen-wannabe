676bb83ca1403d48ec593cb2df60417b144730a8
GiulioSpugnini <giuliospugnini@yahoo.com>
2021-12-20 16:04:05 +0100

    added steps and first step implemented

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+1. Chiedere il nome all'utente
+2. Salvare il nome in una variabile
+3. Chiedere il cognome all'utente
+4. Salvare il cognome in una variabile
+5. Chiedere il colore preferito all-utente
+6. Salvare il colore preferito in una variabile
+7. Sommare le variabili
+*/
+
+// Chiedo il nome all'utente
+const userName = prompt('Come ti chiami?');
+console.log(userName);
